9760352350296c20ecc64ce2fca5dcb0051a911c
Christine Page <sigworld@gmail.com>
2014-03-21 12:51:30 -0700

    added route for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Mapapp::Application.routes.draw do
+  root :to => redirect('/users/sign_up')
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
